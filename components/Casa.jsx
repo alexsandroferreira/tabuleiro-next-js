caf94ab60da13d64ce34ad416d10790e3273ddf6
alexsandroferreira <ferreira.alex@outlook.com>
2021-10-25 18:00:48 -0300

    Criação do componente Casa e casa.module.css

---
+++ components/Casa.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Casa.module.css"
+
+export default function Casa(props) {
+
+    return (
+
+        <div className={styles.casa}></div>
+    )
+
+}
---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+
+import Casa from '../components/Casa'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Casa/>
+      <Casa/>
+      <Casa/>
+
+    </div>
+  )
+}
